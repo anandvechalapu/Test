7796ef34e2885a5b6eca1395e417825b7bd6c828
anandvechalapu <anand.vechalapu@gmail.com>
2023-02-23 18:12:16 +0530

    initial commit

---
+++ class0.9158385993114306.java
@@ -0,0 +1,14 @@
+
+
+Test Suite 1:
+Test Case 1: Verify that the user can enter the required details on the signup page
+Test Case 2: Verify that the user can click the 'Signup' button
+Test Case 3: Verify that the user can redirect to the home page after successful signup
+Test Case 4: Verify that the user can access the various online services available 
+
+Test Suite 2:
+Test Case 1: Verify that the user can enter valid details on the signup page
+Test Case 2: Verify that the user can enter valid email address
+Test Case 3: Verify that the user can enter valid phone number
+Test Case 4: Verify that the user can enter valid address
+Test Case 5: Verify that the accuracy of the results is as expected
