ad6c668ab59104eb08b80df811530e5bd8b6ad38
anandvechalapu <anand.vechalapu@gmail.com>
2023-02-23 18:12:30 +0530

    initial commit

---
+++ class0.22354876671874435.java
@@ -0,0 +1,17 @@
+
+
+Test Cases:
+
+1. Verify that the ‘Signup’ option is available on the home page of the application.
+
+2. Verify that the ‘Individual’ option is available on the ‘Signup’ page of the application.
+
+3. Verify that all required fields are present on the ‘Signup’ page of the application.
+
+4. Verify that the user is able to enter valid details in the required fields.
+
+5. Verify that the user is able to complete the ‘Signup’ process successfully.
+
+6. Verify that after the successful signup, the user is able to access the various online services such as register their vehicles, renew their vehicle registration, check the status of their license, and pay fines etc.
+
+7. Verify that the accuracy of the data entered by the user is correct.
