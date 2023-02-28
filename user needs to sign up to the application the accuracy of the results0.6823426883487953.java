14e16bbf21f7eba4c422730df9f699cc3eba7168
anandvechalapu <anand.vechalapu@gmail.com>
2023-02-28 09:32:41 +0530

    initial commit

---
+++ user needs to sign up to the application the accuracy of the results0.6823426883487953.java
@@ -0,0 +1,11 @@
+
+
+Test Suit 1:
+Test Case 1: Verify ability to enter valid details for signup
+Test Case 2: Verify submission of signup form with valid data
+Test Case 3: Verify that successful sign up should be able to access online services
+
+Test Suit 2:
+Test Case 1: Verify user is not able to submit signup form with invalid data
+Test Case 2: Verify user is not able to submit signup form with incomplete data
+Test Case 3: Verify user is able to view the error message when submitting signup form with invalid data
