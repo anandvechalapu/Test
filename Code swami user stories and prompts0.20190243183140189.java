b29db0f2e0a073b3c9b65c7faaccf82a7d980bd1
anandvechalapu <anand.vechalapu@gmail.com>
2023-02-28 21:14:20 +0530

    initial commit

---
+++ Code swami user stories and prompts0.20190243183140189.java
@@ -0,0 +1,11 @@
+A user manual has been created for the feature.
+A demo video has been created for the feature.
+Spring Boot Application:
+The Spring Boot application should include the following components:
+Controller - The controller will handle user input, validate data, and pass it to services.
+Service - The service will handle the business logic and data access layer.
+Repository - The repository will handle the database access and provide the data to the service.
+The application should include a main class that will start up the application. The main class will be responsible for creating an ApplicationContext and using it to create the controller, service, and repository components. The ApplicationContext will also be used to configure the Jira and GitHub integration.
+The application should also include an API for the user to interact with. The API will be used to enter code generation, select patterns, designs, Jira project details, and GitHub settings. The API will also be used to generate and publish code to GitHub.
+The application should include unit tests for the controller, service, and repository components. The unit tests should cover all acceptance criteria.
+Finally, the application should include an acceptance test that validates the feature against the acceptance criteria. The acceptance test should be run after the unit tests have been successfully executed.
