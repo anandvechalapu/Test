c9b59e24af654ccfeb616ae18f12de4e6e50f5ec
anandvechalapu <anand.vechalapu@gmail.com>
2023-02-28 09:51:42 +0530

    initial commit

---
+++ user needs to sign up to the application the accuracy of the results0.39263660482214835.java
@@ -0,0 +1,11 @@
+
+
+Test Suite 1:
+Test Case 1: Verify the accuracy of the ‘Signup’ form fields.
+Test Case 2: Verify that the user is able to enter valid data and submit the form successfully.
+Test Case 3: Verify that the user is able to access the various online services after signing up.
+
+Test Suite 2:
+Test Case 1: Verify that the ‘Signup’ form fields are mandatory.
+Test Case 2: Verify that the user is able to enter valid data and submit the form successfully with all the required fields.
+Test Case 3: Verify that the user is able to access the various online services after signing up with valid credentials.
