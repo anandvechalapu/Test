01eb916b56bbc15a155aa309982c4d12e7a64361
anandvechalapu <anand.vechalapu@gmail.com>
2023-02-28 09:33:20 +0530

    initial commit

---
+++ user needs to sign up to the application the accuracy of the results0.7063640077048532.java
@@ -0,0 +1,19 @@
+
+
+Test Suit 1:
+Verify that user is able to enter the required fields of the Signup form correctly and accurately.
+
+Test Suit 2:
+Verify that user is able to register as a new user successfully.
+
+Test Suit 3:
+Verify that user is able to access various online services after registering successfully.
+
+Test Suit 4:
+Verify that user is able to register his/her vehicles and renew their vehicle registration successfully.
+
+Test Suit 5:
+Verify that user is able to check the status of his/her license successfully.
+
+Test Suit 6:
+Verify that user is able to pay fines successfully.
