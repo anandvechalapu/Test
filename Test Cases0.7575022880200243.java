c87641316f03f00fd434c5d2e15d672507bb96ef
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-01 10:45:38 +0530

    initial commit

---
+++ Test Cases0.7575022880200243.java
@@ -0,0 +1,8 @@
+1 Verify the application is able to create a new user
+Navigate to the application home page
+N/A
+The user should be able to create a new user on the application home page
+2 Verify the application is able to retrieve an existing user
+Navigate to the application home page
+An existing user should be present in the system
+The user should be able to retrieve the existing user from the application home page.
