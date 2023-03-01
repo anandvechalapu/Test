ab758657c0db425ed546cc80cb090c01d842bda3
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-01 18:17:50 +0530

    initial commit

---
+++ Management 0.10750143281742741.java
@@ -0,0 +1,14 @@
+·       User should be able to see and access the sub screens of management functionality.
+·       User should be able to create, edit, view the sub screens.
+
+Solution:
+This application can be developed using Spring Boot. The application will contain a controller, service and repository classes for the user story.
+
+Controller:
+The controller will handle the user requests and calls the appropriate services. It will validate the user input and return the appropriate response.
+
+Service:
+The service layer will contain the business logic for the user story. It will handle the requests from the controller and call the repository layer for database operations.
+
+Repository:
+The repository layer will handle the database operations. It will contain methods to create, edit, view the sub screens of the management functionality.
