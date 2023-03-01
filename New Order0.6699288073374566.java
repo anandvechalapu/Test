d3bee36a0d190f13305dd9dabc401c6c87903d3a
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-01 17:34:00 +0530

    initial commit

---
+++ New Order0.6699288073374566.java
@@ -0,0 +1,17 @@
+
+Steps to generate Spring boot application with controller, service and repository classes for the given user story:
+1. Create a Spring Boot project with the help of Spring Initializer.
+2. Create a controller class with methods to access the functionalities displayed in the CSA new order function.
+3. Create a service class with methods to click all functionalities on the new order menu.
+4. Create a repository class to store the data related to the new order.
+5. Create a search method in the service class to search by account number.
+6. Create methods to display all functionalities on the new order menu with the following details: retailer name, trading as, account number, phone number, address, country, and status.
+7. Create methods for buyer action to display the following information: login ID, buyer name, account number, retailer name, email address, mobile number, and phone number.
+8. Create methods for the user to go next or back to the previous action.
+9. Create methods for the user to add order or add buyer option.
+10. Create methods for the user to search product by product name or category.
+11. Create methods to display the following menu once product add on new order: banner, product name, product code, items in stock, units per pack, retailer cost price, and minimum pack order quantity.
+12. Create methods for the user to add product on shopping cart and go to checkout process.
+13. Create methods to display the details on checkout action with following: product name, items in stock (packs), units per pack, quantity pack, quantity units, retailer cost price, line item cost, line item VAT, line item total cost, order total cost, update/process order, and cancel order.
+14. Create methods for the user to update/process or cancel the order.
+15. Create methods to ensure the user interface is responsive, easy to navigate, and secure.
