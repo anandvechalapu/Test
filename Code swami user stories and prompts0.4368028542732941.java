95bb8e86e82f4292091dba0f48a22c2e5463b4f8
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-01 13:11:41 +0530

    initial commit

---
+++ Code swami user stories and prompts0.4368028542732941.java
@@ -0,0 +1,8 @@
+The code has been documented and reviewed by the team.
+Spring Boot Application
+
+For the given user story, a Spring Boot application would be created with controller, service and repository classes. The Controller class would be responsible for handling the user input and validating it. The Service class would be responsible for generating the code based on the user's selections, and the Repository class would be responsible for managing the data and providing access to it.
+
+The application would also include a main class for running the application and a configuration class for setting up the Spring Boot application. The application would also include unit tests for each of the controller, service and repository classes.
+
+The application would also include integration tests for testing the Jira and GitHub integrations. Additionally, the application would include integration tests for testing the code generation and publishing to GitHub. Finally, the application would be documented and the code reviewed by the team before the feature is released.
