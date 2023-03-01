0a17604e2fc5bea161541986f36b39cb4e9349ad
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-01 23:03:32 +0530

    initial commit

---
+++ null0.7271720903999852.java
@@ -0,0 +1,16 @@
+
+
+User Story: As a user, I should be able to register on the website
+
+Test Suite:
+
+1. Verify that all required fields are present on the registration form.
+2. Verify that the form is accessible on all browsers.
+3. Verify that the user can enter valid data in all the fields.
+4. Verify that the user is able to select the gender.
+5. Verify that the user can enter a valid email address.
+6. Verify that the user is able to upload an image/profile picture.
+7. Verify that the password entered by the user is valid and secure.
+8. Verify that the user is able to submit the registration form.
+9. Verify that the user can log in with the registered details.
+10. Verify that the registration page is secured against SQL injection.
